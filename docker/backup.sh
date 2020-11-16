d83634128d166d4d309b17f8250c632863ff52ff
Leo <leo.lou@gov.bc.ca>
2020-11-16 12:53:05 -0800

    Update backup.sh

--- docker/backup.sh
+++ docker/backup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# add postgres user patch for openshift
+if [[ $UID -ge 10000 ]]; then
+    GID=$(id -g)
+    sed -e "s/^postgres:x:[^:]*:[^:]*:/postgres:x:$UID:$GID:/" /etc/passwd > /tmp/passwd
+    cat /tmp/passwd > /etc/passwd
+    rm /tmp/passwd
+fi
+
 # ======================================================================================
 # Imports
 # --------------------------------------------------------------------------------------
